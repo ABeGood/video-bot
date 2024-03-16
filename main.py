748f24dc1a199328faccd5c1cb5701ed4238dd42
ABeGood <openzzggl@gmail.com>
2024-03-16 21:11:56 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import config
+import time
+
+from scenedetect import detect, AdaptiveDetector, split_video_ffmpeg
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
+from pytube import YouTube
+from os import listdir, remove, replace, path, makedirs
+from os.path import isfile, join
+
+
+DOWNLOAD_NAME = 'downloaded_video.mp4'
+
+DOWNLOAD_PATH = 'videos/downloaded'
+PROCESSED_PATH = 'videos/processed'
+TO_POST_PATH = 'videos/to_post'
+
+path_list = [DOWNLOAD_PATH, PROCESSED_PATH, TO_POST_PATH]
+
+for path_to_check in path_list:
+    if not path.exists(path_to_check): 
+        makedirs(path_to_check)
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+async def cut(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print(f'Loading {context.args[0]}')
+    link = context.args[0]
+    video = YouTube(link)
+    video.streams.filter(progressive = True, file_extension = "mp4").first().download(output_path = DOWNLOAD_PATH, filename = DOWNLOAD_NAME)
+
+    scene_list = detect(f'{DOWNLOAD_PATH}/{DOWNLOAD_NAME}', AdaptiveDetector())
+    split_video_ffmpeg(f'{DOWNLOAD_PATH}/{DOWNLOAD_NAME}', scene_list, output_dir=PROCESSED_PATH, show_progress=True)
+
+    onlyfiles = [f for f in listdir(PROCESSED_PATH) if isfile(join(PROCESSED_PATH, f))]
+
+    for file in onlyfiles:
+        keyboard = [
+            [
+                InlineKeyboardButton("✅", callback_data="1"),
+                InlineKeyboardButton("❌", callback_data="0"),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        with open(f'{PROCESSED_PATH}/{file}', 'rb') as file:
+            await bot.send_video(config.test_chat_id, video=file, reply_markup=reply_markup, filename=f'{PROCESSED_PATH}/{file}')
+            time.sleep(0.2)
+
+    # remove(f'{DOWNLOAD_PATH}/{DOWNLOAD_NAME}')
+    # for file in onlyfiles:
+    #     remove(f'{PROCESSED_PATH}/{file}')
+
+    await update.message.reply_text(f'loaded {context.args[0]}')
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    # print(update.message.video.file_name)
+    query = update.callback_query
+
+    path_to_delete = (f'{PROCESSED_PATH}/{query.message.effective_attachment.file_name}')
+
+    if query.data == "0":
+        if path.exists(path_to_check): 
+            remove(path_to_delete)
+        await query.delete_message()
+    elif query.data == '1':
+        replace(f'{PROCESSED_PATH}/{query.message.effective_attachment.file_name}', f'{TO_POST_PATH}/{query.message.effective_attachment.file_name}')
+        await query.edit_message_caption(caption='✅ Saved for later post.')
+
+    await query.answer()
+
+    # await query.edit_message_text(text=f"Selected option: {query.data}")
+
+
+bot = Bot(config.token)
+
+app = ApplicationBuilder().token(config.token).build()
+
+app.add_handler(CallbackQueryHandler(button))
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("cut", cut))
+
+app.run_polling()
---
+++ tg_api.py
@@ -0,0 +1,84 @@
+import requests
+import config
+import json
+from telegram.ext import Application, CommandHandler, CallbackContext, ContextTypes
+from telegram import Update
+
+
+# TODO: One text with multiple messages, maybe will need to switch to tg lib from REST API
+
+def post(message: str, channels: list, image_path=None):
+
+    get_post_type = {
+        str: 'text',
+        tuple: 'media'
+    }
+
+    for channel in channels:
+        if image_path is not None:
+            if get_post_type[type(image_path)] == 'photo':
+                url = f"https://api.telegram.org/bot{config.token}/sendPhoto"
+                files = {'photo': open(image_path, 'rb')}
+                data = {'chat_id': channel, 'caption': message}
+
+            elif get_post_type[type(image_path)] == 'media':
+                url = f"https://api.telegram.org/bot{config.token}/sendMediaGroup"
+                files = {}
+                media = []
+                for i, attachment in enumerate(image_path):
+                    files[f'image{i}'] = open(attachment, 'rb')
+
+                    media.append(
+                        {'type': 'photo', 'media': f'attach://image{i}', 'caption': f'Caption for image {i+1}'}
+                    )
+
+                data = {'chat_id': channel, 'media': json.dumps(media)}
+
+        else:
+            url = f"https://api.telegram.org/bot{config.token}/sendMessage"
+            files = None
+            data = {'chat_id': channel, 'text': message, 'parse_mode': 'HTML'}
+
+        response = requests.post(url, files=files, data=data)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            print("Post sent successfully")
+        else:
+            print(f"Failed to send photo. Status code: {response.status_code}")
+
+
+async def respond_post(update: Update,
+                        context: ContextTypes.DEFAULT_TYPE) -> None:
+    print('Got message...')
+    # link = update.effective_message.text.split(' ')[-1]
+    # print(f'Link: {link}')
+    # # bot replies to the message sending a string with the username
+    # column = scrapper.scrap_post(link)
+    # print(f'Column scrapped')
+    # processed_text = gpt.process_text_with_gpt(column['text'])['choices'][0]['message']['content']
+    # processed_text = processed_text.removeprefix("'").removesuffix("'")
+    # column_text = json.loads(processed_text)
+    # print(f'Text processed')
+    # img_links = ', '.join(column['img_links'])
+    # image_link = gpt.send_images_and_get_relevance(processed_text=processed_text, images=img_links)['choices'][0]['message']['content']
+    # image_link = f'<a href="{image_link}">⁠</a>'
+    # print(f'Image ink: {image_link}')
+    # post_text = f'<b>{column_text['title']}</b>\n\n{column_text['text']}\n{image_link}'
+
+    # # logger.save_post(column_text['title'], post_text)
+    # # post_text = f'<b>{column_text['title']}</b>\n\n{column_text['text']}\n\nИсточник: {link}\n{image_link}'
+    # # extension = image_link.split('.')[-1]
+    # # with open(f'posts/img/img.{extension}', "wb") as f:
+    # #     f.write(requests.get(image_link).content)
+
+    # post(message=post_text, channels=[config.test_chat_id])
+    # os.remove("posts/img/img.jpg")
+
+# post(message='Media group', channels=[config.test_chat_id], image_path=('fans.png', 'match.png'))
+
+# bot = Application.builder().token(config.token).build()
+# bot.add_handler(CommandHandler("post", respond_post))
+
+# # start and poll for updates, press CRTL+C to stop
+# bot.run_polling()
